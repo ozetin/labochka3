59c5abf61c052d0671bf263ce09490763e94a5e7
Вадим <vadimkairov20@gmail.com>
2025-10-08 01:16:20 +0300

    корректировка задания 2

--- zadanie2.py
+++ zadanie2.py
@@ -4,14 +4,13 @@ c1 = int(input("Введите значение c1: "))
 a2 = int(input("Введите значение а2: "))
 b2 = int(input("Введите значение b2: "))
 c2 = int(input("Введите значение c2: "))
-if a1 == a2:
-    if b1 == b2 and c1 == c2:
-        print("Прямые совпадают")
-    elif a1 == 0 and b1 ==0 and c1 != 0:
-        print("Прямой 1 не существует")
-    elif a2 == 0 and b2 == 0 and c2 != 0:
-        print("Прямой 2 не существует")
-    elif b1 == b2:
+if a1 == a2 and b1 == b2 and c1 == c2:
+    print("Прямые совпадают")
+elif a1 == 0 and b1 ==0 and c1 != 0:
+    print("Прямой 1 не существует")
+elif a2 == 0 and b2 == 0 and c2 != 0:
+    print("Прямой 2 не существует")
+elif a1==a2 and b1 == b2:
         print("Прямые параллельны")
 else:
     a1_2 = a1 / b1
