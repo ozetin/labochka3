8db98cd765178c3525c79371c5d7b89c720c68cc
Вадим <vadimkairov20@gmail.com>
2025-10-13 11:36:32 +0300

    ЗаPEPил задания

--- Задания/zadanie2.py
+++ Задания/zadanie2.py
@@ -1,24 +1,35 @@
-a1 = int(input("Введите значение а1: "))
-b1 = int(input("Введите значение b1: "))
-c1 = int(input("Введите значение c1: "))
-a2 = int(input("Введите значение а2: "))
-b2 = int(input("Введите значение b2: "))
-c2 = int(input("Введите значение c2: "))
-if a1 == a2 and b1 == b2 and c1 == c2:
-    print("Прямые совпадают")
-elif a1 == 0 and b1 ==0 and c1 != 0:
-    print("Прямой 1 не существует")
-elif a2 == 0 and b2 == 0 and c2 != 0:
-    print("Прямой 2 не существует")
-elif a1==a2 and b1 == b2:
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    a1 = int(input("Введите значение a1: "))
+    b1 = int(input("Введите значение b1: "))
+    c1 = int(input("Введите значение c1: "))
+
+    a2 = int(input("Введите значение a2: "))
+    b2 = int(input("Введите значение b2: "))
+    c2 = int(input("Введите значение c2: "))
+
+    if a1 == a2 and b1 == b2 and c1 == c2:
+        print("Прямые совпадают")
+
+    elif a1 == 0 and b1 == 0 and c1 != 0:
+        print("Прямая 1 не существует")
+
+    elif a2 == 0 and b2 == 0 and c2 != 0:
+        print("Прямая 2 не существует")
+
+    elif a1 == a2 and b1 == b2:
         print("Прямые параллельны")
-else:
-    a1_2 = a1 / b1
-    c1_2 = c1 / b1
-    a2_2 = a2 / b2
-    c2_2 = c2 / b2
-    a = a1_2 - a2_2
-    c = c1_2 - c2_2
-    x = -c / a
-    y = -(a1 * x + c1) / b1
-    print(x, y)
+
+    else:
+        k1 = -a1 / b1
+        b1_new = -c1 / b1
+
+        k2 = -a2 / b2
+        b2_new = -c2 / b2
+
+        x = (b2_new - b1_new) / (k1 - k2)
+        y = k1 * x + b1_new
+
+        print(f"Точка пересечения: x = {x}, y = {y}")
--- Задания/zadanie3.py
+++ Задания/zadanie3.py
@@ -1,5 +1,10 @@
-for i in range(10,100):
-    summ = i%10 + i//10
-    summ += summ**2
-    if summ == i:
-        print(i)
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    for i in range(10, 100):
+        summ = i % 10 + i // 10
+        summ += summ ** 2
+
+        if summ == i:
+            print(i)
